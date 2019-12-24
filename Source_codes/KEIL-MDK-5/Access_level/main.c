342fa24707140c2683ecc06d9e76e366f6ffd061
Ankit Rana <futechiot@gmail.com>
2019-12-24 12:23:55 +0530

    Update main.c

--- Source_codes/KEIL-MDK-5/Access_level/main.c
+++ Source_codes/KEIL-MDK-5/Access_level/main.c
@@ -8,8 +8,8 @@ void WWDG_IRQHandler(void)
 	for(int i=0 ; i < 50 ; i++);
 	
 		/* Lets try to move the processor to privileged access level */
-	uint32_t value = __get_CONTROL();
-	value &= ~0x01;
+	uint32_t value = __get_CONTROL();   // we are in interrupt handling function, means in Handler mode, now we can 
+	value &= ~0x01;                     //change CONTROL REGISTER and clear its first bit to go into piviledge mode  
 	__set_CONTROL(value);
 	
 }
@@ -27,10 +27,12 @@ void call_application_Task(void)
 	
 	/* Lets try to move the processor to privileged access level */
 	uint32_t value = __get_CONTROL();
-	value &= ~0x01;
-	__set_CONTROL(value);
+	value &= ~0x01; 
+	__set_CONTROL(value); // Not going into priviledge Access level cause "code is in Thread mode and in unpreviledge mode"
+                              // so it cant excess(write) the CONTROL register that we are changing to go into priviledge Access level
 	
-	generate_interrupt();
+	generate_interrupt(); // we are only able to go into PAL from NPAL, by going into HAndler mode(executing any interrupt or exception)
+                              //and in this interrupts handler function we can smartly place our CONTROL REGISTER clearing code. 
 }
 
 void RTOS_init(void)
